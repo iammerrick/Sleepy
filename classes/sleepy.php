f2c815d623727189a7c6031ce0efef37ee9d676d
Merrick Christensen <merrick.christensen@gmail.com>
2011-04-13 13:42:40 -0600

    Initial commit.

---
+++ classes/sleepy.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+* Sleepy
+*/
+class Sleepy extends Sleepy_Core
+{
+	
+}
---
+++ classes/sleepy/core.php
@@ -0,0 +1,134 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Sleepy_Core extends Model {
+	
+	protected $_data_url = '';
+	protected $_data = array();
+	
+	const STATE_NEW = 'new';
+	const STATE_LOADED = 'loaded';
+	const STATE_UPDATED = 'updated';
+	
+	protected $_status = Sleepy_Core::STATE_NEW;
+	
+	public function load($url = NULL)
+	{
+		$request = $this->get_request($url);
+		$response = $request->execute();
+		
+		if($response->status() === 200)
+		{
+			return $this->load_data($response->body());
+		}
+		else
+		{
+			throw new Kohana_Exception('Sleepy Request Failed');
+		}
+	}
+	
+	protected function load_data($data)
+	{
+		$decoded = json_decode($data);
+		
+		foreach($decoded as $key => $value)
+		{
+			if(array_key_exists($key, $this->_data))
+			{
+				$this->_data[$key] = $value;
+			}
+		}
+		
+		$this->_status = Sleepy_Core::STATE_LOADED;
+		return $this;
+	}
+	
+	public function save($url = NULL)
+	{
+	    return $this->loaded() ? $this->update($url) : $this->create($url);
+	}
+	
+	public function update($url = NULL)
+	{
+		$request = $this->get_request($url);
+		$request->method('POST');
+		$request->post($this->_data);
+		$request->execute();
+		
+		$this->_status = Sleepy_Core::STATE_LOADED;
+	}
+	
+	public function create($url = NULL)
+	{
+		$request = $this->get_request($url);
+		$request->method('POST');
+		$request->post(json_encode($this->_data));
+		$response = $request->execute();
+		
+		$this->load_data($response->body());
+		
+		$this->_status = Sleepy_Core::STATE_LOADED;
+	}
+	
+	public function set_fields(array $data, array $keys = NULL)
+	{
+		if(is_array($keys))
+		{
+			$data = Arr::extract($data, $keys);
+		}
+
+		foreach (array_intersect_key($data, $this->_data) as $key => $value)
+		{
+			$this->$key = $value;
+		}
+		
+		return $this;
+	}
+	
+	public function loaded()
+	{
+		return $this->_status === Sleepy_Core::STATE_LOADED;
+	}
+	
+	private function get_request($url = NULL)
+	{
+		if($url === NULL)
+		{
+			$url = $this->_data_url;
+		}
+		
+		return Request::factory($url);
+	}
+	
+	public function __get($key)
+	{
+		if (array_key_exists($key, $this->_data))
+		{
+		 	return $this->_data[$key];
+		}
+
+		throw new Kohana_Exception('Field '.$key.' does not exist in '.get_class($this).'!', array(), '');
+	}
+	
+	public function __set($key, $value)
+	{
+		if (array_key_exists($key, $this->_data))
+		{
+			$this->_data[$key] = $value;
+			$this->_state = Sleepy_Core::STATE_UPDATED;
+			return;
+		}
+		
+	}
+	
+	public function __isset($name)
+	{
+		return isset($this->_data[$name]);
+	}
+	
+	public function as_array()
+	{
+		return $this->_data;
+	}
+	
+	
+} // End Sleepy_Core
